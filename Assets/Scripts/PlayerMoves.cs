7f7f79cf9430d952be7186d3be206f24e772e0a8
JohnMohring <johnmmohring@gmail.com>
2026-06-20 14:21:08 -0400

    Basic Character & Camera Controls

---
+++ Assets/Scripts/PlayerMoves.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.ComponentModel;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlayerMoves : MonoBehaviour
+{
+    public InputActionAsset InputActions;
+
+    InputAction move;
+    InputAction look;
+    InputAction jump;
+    InputAction sprint;
+    InputAction pause;
+
+    public Vector2 moveAmt;
+    public Vector2 lookAmt;
+
+    public Rigidbody rb;
+
+    public float moveSpeed = 1;
+
+    public float lookSpeed = 1;
+    public float jumpSpeed = 1;
+    public float airSpeed = .1f;
+
+    public bool grounded = false;
+
+    public bool canPause = true;
+
+    private float xCamRotation = 0f;
+
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+    }
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        // Get InputAction references from Project-wide input actions.
+        if (InputSystem.actions)
+        {
+            move = InputSystem.actions.FindAction("Player/Move");
+            look = InputSystem.actions.FindAction("Player/Look");
+            jump = InputSystem.actions.FindAction("Player/Jump");
+            sprint = InputSystem.actions.FindAction("Player/Sprint");
+            pause = InputSystem.actions.FindAction("Player/Pause");
+        }
+
+
+
+    }
+
+    private void Update()
+    {
+
+        moveAmt = move.ReadValue<Vector2>();
+        lookAmt = look.ReadValue<Vector2>();
+
+
+
+
+        Ray ray = new Ray(transform.position, -transform.up);
+        Physics.Raycast(ray, out RaycastHit hit, 1.25f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+
+        if (hit.collider != null)
+        {
+            grounded = true;
+            rb.linearDamping = 5f;
+        }
+        else
+        {
+            grounded = false;
+            rb.linearDamping = .05f;
+        }
+
+
+
+        if (jump.WasPressedThisFrame())
+        {
+            DoJump();
+        }
+
+     
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        Walking();
+        Rotating();
+
+    }
+
+
+    private void Walking()
+    {
+        rb.AddForce(transform.forward * moveAmt.y * moveSpeed, ForceMode.VelocityChange);
+        rb.AddForce(transform.right * moveAmt.x * moveSpeed, ForceMode.VelocityChange);
+    }
+
+
+    private void Rotating()
+    {
+        transform.Rotate(Vector3.up, lookAmt.x * lookSpeed);
+
+        xCamRotation -= lookAmt.y;
+        xCamRotation = Mathf.Clamp(xCamRotation, -90f, 90f);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(xCamRotation, 0f, 0f);
+
+    }
+
+    public void DoJump()
+    {
+        Debug.Log("jump");
+        if(grounded)
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+    }
+
+}
